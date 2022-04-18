14592fa63e205d125da4fce9c0d6f3fdc324bd1f
Tom Drabas <drabas.t@gmail.com>
2022-04-18 22:05:55 +0000

    Working example w/SQLITE

--- src/flight_sql.cpp
+++ src/flight_sql.cpp
@@ -1,13 +1,147 @@
+#include <cstdlib>
 #include <iostream>
-#include <duckdb.hpp>
-#include <sqlite3.h>
+#include <pthread.h>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Flight SQL Example of running queries against "
-                 "SQLite and DuckDB." << std::endl;
+#include <arrow/flight/client.h>
+#include <arrow/flight/sql/client.h>
+#include <arrow/table.h>
 
-    duckdb::DuckDB db = duckdb::DuckDB(nullptr);
-    duckdb::Connection con = duckdb::Connection(db);
+#include "sqlite/sqlite_server.h"
 
-    return 0;
+namespace flight = arrow::flight;
+namespace flightsql = arrow::flight::sql;
+
+int port = 31337;
+
+void* CreateServer(void *id) {
+    std::cout << "Creating server in thread: " << (long) id << std::endl;
+
+    auto location_result = flight::Location::ForGrpcTcp("localhost", port);
+    arrow::flight::Location location;
+    if (location_result.ok()) {
+        location = std::move(location_result).ValueOrDie();
+    } else {
+        std::cout << "Error occured when assigning location. Stopping.";
+        return (void*) 1;
+    }
+
+    arrow::flight::FlightServerOptions options(location);
+    auto server_result = arrow::flight::sql::sqlite::SQLiteFlightSqlServer::Create();
+    std::shared_ptr<arrow::flight::sql::sqlite::SQLiteFlightSqlServer> server;
+
+    if (server_result.ok()) {
+        server = std::move(server_result).ValueOrDie();
+    } else {
+        std::cout << "Error occured when assigning location. Stopping.";
+        return (void*) 1;
+    }
+
+    if (!server->Init(options).ok()) {
+        std::cout << "Error occured when assigning location. Stopping.";
+        return (void*) 1;
+    }
+    // // Exit with a clean error code (0) on SIGTERM
+    if (!server->SetShutdownOnSignals({SIGTERM}).ok()) {
+        std::cout << "Error occured when assigning location. Stopping.";
+        return (void*) 1;
+    }
+
+    std::cout << "Server listening on localhost:" << server->port() << std::endl;
+    if (!server->Serve().ok()) {
+        std::cout << "Error occured when assigning location. Stopping.";
+        return (void*) 1;
+    }
+
+    // return arrow::Status::OK();
+    return (void*) 0;
+}
+
+// arrow::Status Main() {
+void* CreateClient(void *id) {
+//   ARROW_ASSIGN_OR_RAISE(auto location,
+//                         flight::Location::ForGrpcTcp("localhost", 31337));
+    std::cout << "Creating client in thread: " << (long) id << std::endl;
+
+    auto location_result = flight::Location::ForGrpcTcp("localhost", port);
+    arrow::flight::Location location;
+    if (location_result.ok()) {
+        location = std::move(location_result).ValueOrDie();
+    } else {
+        std::cout << "Error occured when assigning location. Stopping.";
+        return (void*) 1;
+    }
+    std::cout << "Connecting to " << location.ToString() << std::endl;
+
+    const std::string kQuery = "SELECT 1";
+
+    // Set up the Flight SQL client
+    auto flight_client_result = flight::FlightClient::Connect(location);
+    std::unique_ptr<flight::FlightClient> flight_client;
+    
+    // ARROW_ASSIGN_OR_RAISE(flight_client, flight::FlightClient::Connect(location));
+    if (flight_client_result.ok()) {
+        flight_client = std::move(flight_client_result.ValueOrDie());
+    }
+    std::unique_ptr<flightsql::FlightSqlClient> client(
+        new flightsql::FlightSqlClient(std::move(flight_client)));
+
+    flight::FlightCallOptions call_options;
+
+    // Execute the query, getting a FlightInfo describing how to fetch the results
+    std::cout << "Executing query: '" << kQuery << "'" << std::endl;
+    auto flight_info_result = client->Execute(call_options, kQuery);
+    std::unique_ptr<flight::FlightInfo> flight_info;
+
+    if (flight_info_result.ok()) {
+        flight_info = std::move(flight_info_result.ValueOrDie());
+    }
+    //   ARROW_ASSIGN_OR_RAISE(std::unique_ptr<flight::FlightInfo> flight_info,
+    //                         client->Execute(call_options, kQuery));
+
+      // Fetch each partition sequentially (though this can be done in parallel)
+      for (const flight::FlightEndpoint& endpoint : flight_info->endpoints()) {
+        // Here we assume each partition is on the same server we originally queried, but this
+        // isn't true in general: the server may split the query results between multiple
+        // other servers, which we would have to connect to.
+
+        // The "ticket" in the endpoint is opaque to the client. The server uses it to
+        // identify which part of the query results to return.
+        auto stream_result = client->DoGet(call_options, endpoint.ticket);
+        std::unique_ptr<arrow::flight::FlightStreamReader> stream;
+        if (stream_result.ok()) {
+            stream = std::move(stream_result.ValueOrDie());
+        }
+        // ARROW_ASSIGN_OR_RAISE(auto stream, client->DoGet(call_options, endpoint.ticket));
+        // Read all results into an Arrow Table, though we can iteratively process record
+        // batches as they arrive as well
+        auto table_result = stream->ToTable();
+        auto table = std::move(table_result.ValueOrDie());
+
+
+
+        // ARROW_ASSIGN_OR_RAISE(auto table, stream->ToTable());
+        std::cout << "Read one chunk:" << std::endl;
+        std::cout << table->ToString() << std::endl;
+      }
+    // return (void*) 0;
+    pthread_exit(NULL);
+}
+
+int main(int argc, char** argv) {
+    pthread_t threads[2]; // two threads: server and client
+    void* result_server; 
+    void* result_client;
+
+    int server_t = pthread_create(&threads[0], NULL, CreateServer, (void*) 0);
+    usleep(2000); // let the server start up
+
+    int client_t = pthread_create(&threads[1], NULL, CreateClient, (void*) 1);
+
+    if (client_t != 0) {
+        return EXIT_FAILURE;
+    }
+
+    pthread_join(threads[0], &result_server);
+    pthread_join(threads[1], &result_client);
+    return EXIT_SUCCESS;
 }
--- src/sqlite/sqlite_type_info.cc
+++ src/sqlite/sqlite_type_info.cc
@@ -21,6 +21,26 @@
 #include <arrow/flight/sql/types.h>
 #include <arrow/record_batch.h>
 #include <arrow/testing/gtest_util.h>
+#include "arrow/testing/json_integration.h"
+
+
+// #include <arrow/array.h>
+// #include <arrow/array/concatenate.h>
+// #include <arrow/chunked_array.h>
+// #include <arrow/io/memory.h>
+// #include <arrow/result.h>
+// #include <arrow/testing/gtest_util.h>
+// #include <arrow/type_fwd.h>
+// #include <arrow/util/checked_cast.h>
+// #include <arrow/util/logging.h>
+
+// #include <parquet/arrow/reader.h>
+// #include <parquet/arrow/schema.h>
+// #include <parquet/column_writer.h>
+// #include <parquet/file_writer.h>
+// #include <parquet/properties.h>
+
+// using arrow::ArrayFromJSON;
 
 namespace arrow {
 namespace flight {
@@ -28,59 +48,60 @@ namespace sql {
 namespace sqlite {
 
 std::shared_ptr<RecordBatch> DoGetTypeInfoResult() {
-  auto type_name_array =
-      ArrayFromJSON(utf8(), R"(["bit", "tinyint", "bigint", "longvarbinary",
-                            "varbinary", "text", "longvarchar", "char",
-                            "integer", "smallint", "float", "double",
-                            "numeric", "varchar", "date", "time",
-                            "timestamp"])");
-  auto data_type = ArrayFromJSON(
-      int32(), R"([-7, -6, -5, -4, -3, -1, -1, 1, 4, 5, 6, 8, 8, 12, 91, 92, 93])");
-  auto column_size = ArrayFromJSON(
-      int32(),
-      R"([1, 3, 19, 65536, 255, 65536, 65536, 255, 9, 5, 7, 15, 15, 255, 10, 8, 32])");
-  auto literal_prefix = ArrayFromJSON(
-      utf8(),
-      R"([null, null, null, null, null, "'", "'", "'", null, null, null, null, null, "'", "'", "'", "'"])");
-  auto literal_suffix = ArrayFromJSON(
-      utf8(),
-      R"([null, null, null, null, null, "'", "'", "'", null, null, null, null, null, "'", "'", "'", "'"])");
-  auto create_params = ArrayFromJSON(
-      list(field("item", utf8(), false)),
-      R"([[], [], [], [], [], ["length"], ["length"], ["length"], [], [], [], [], [], ["length"], [], [], []])");
-  auto nullable =
-      ArrayFromJSON(int32(), R"([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])");
-  // Reference for creating a boolean() array only with zero.
-  auto zero_bool_array =
-      ArrayFromJSON(boolean(), R"([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])");
-  const auto& case_sensitive = zero_bool_array;
-  auto searchable =
-      ArrayFromJSON(int32(), R"([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3])");
-  const auto& unsigned_attribute = zero_bool_array;
-  const auto& fixed_prec_scale = zero_bool_array;
-  const auto& auto_unique_value = zero_bool_array;
-  auto local_type_name =
-      ArrayFromJSON(utf8(), R"(["bit", "tinyint", "bigint", "longvarbinary",
-                          "varbinary", "text", "longvarchar", "char",
-                          "integer", "smallint", "float", "double",
-                          "numeric", "varchar", "date", "time",
-                          "timestamp"])");
-  // Reference for creating an int32() array only with zero.
-  auto zero_int_array =
-      ArrayFromJSON(int32(), R"([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])");
-  const auto& minimal_scale = zero_int_array;
-  const auto& maximum_scale = zero_int_array;
-  const auto& sql_data_type = data_type;
-  const auto& sql_datetime_sub = zero_int_array;
-  const auto& num_prec_radix = zero_int_array;
-  const auto& interval_precision = zero_int_array;
+//   auto type_name_array =
+    //   arrow::ArrayFromJSON(utf8(), R"(["bit", "tinyint", "bigint", "longvarbinary",
+    //                         "varbinary", "text", "longvarchar", "char",
+    //                         "integer", "smallint", "float", "double",
+    //                         "numeric", "varchar", "date", "time",
+    //                         "timestamp"])");
+//   auto data_type = ArrayFromJSON(
+//       int32(), R"([-7, -6, -5, -4, -3, -1, -1, 1, 4, 5, 6, 8, 8, 12, 91, 92, 93])");
+//   auto column_size = ArrayFromJSON(
+//       int32(),
+//       R"([1, 3, 19, 65536, 255, 65536, 65536, 255, 9, 5, 7, 15, 15, 255, 10, 8, 32])");
+//   auto literal_prefix = ArrayFromJSON(
+//       utf8(),
+//       R"([null, null, null, null, null, "'", "'", "'", null, null, null, null, null, "'", "'", "'", "'"])");
+//   auto literal_suffix = ArrayFromJSON(
+//       utf8(),
+//       R"([null, null, null, null, null, "'", "'", "'", null, null, null, null, null, "'", "'", "'", "'"])");
+//   auto create_params = ArrayFromJSON(
+//       list(field("item", utf8(), false)),
+//       R"([[], [], [], [], [], ["length"], ["length"], ["length"], [], [], [], [], [], ["length"], [], [], []])");
+//   auto nullable =
+//       ArrayFromJSON(int32(), R"([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])");
+//   // Reference for creating a boolean() array only with zero.
+//   auto zero_bool_array =
+//       ArrayFromJSON(boolean(), R"([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])");
+//   const auto& case_sensitive = zero_bool_array;
+//   auto searchable =
+//       ArrayFromJSON(int32(), R"([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3])");
+//   const auto& unsigned_attribute = zero_bool_array;
+//   const auto& fixed_prec_scale = zero_bool_array;
+//   const auto& auto_unique_value = zero_bool_array;
+//   auto local_type_name =
+//       ArrayFromJSON(utf8(), R"(["bit", "tinyint", "bigint", "longvarbinary",
+//                           "varbinary", "text", "longvarchar", "char",
+//                           "integer", "smallint", "float", "double",
+//                           "numeric", "varchar", "date", "time",
+//                           "timestamp"])");
+//   // Reference for creating an int32() array only with zero.
+//   auto zero_int_array =
+//       ArrayFromJSON(int32(), R"([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])");
+//   const auto& minimal_scale = zero_int_array;
+//   const auto& maximum_scale = zero_int_array;
+//   const auto& sql_data_type = data_type;
+//   const auto& sql_datetime_sub = zero_int_array;
+//   const auto& num_prec_radix = zero_int_array;
+//   const auto& interval_precision = zero_int_array;
 
-  return RecordBatch::Make(
-      SqlSchema::GetXdbcTypeInfoSchema(), 17,
-      {type_name_array, data_type, column_size, literal_prefix, literal_suffix,
-       create_params, nullable, case_sensitive, searchable, unsigned_attribute,
-       fixed_prec_scale, auto_unique_value, local_type_name, minimal_scale, maximum_scale,
-       sql_data_type, sql_datetime_sub, num_prec_radix, interval_precision});
+//   return RecordBatch::Make(
+//       SqlSchema::GetXdbcTypeInfoSchema(), 17,
+//       {type_name_array, data_type, column_size, literal_prefix, literal_suffix,
+//        create_params, nullable, case_sensitive, searchable, unsigned_attribute,
+//        fixed_prec_scale, auto_unique_value, local_type_name, minimal_scale, maximum_scale,
+//        sql_data_type, sql_datetime_sub, num_prec_radix, interval_precision});
+    return RecordBatch::MakeEmpty(SqlSchema::GetXdbcTypeInfoSchema()).ValueOrDie();
 }
 
 std::shared_ptr<RecordBatch> DoGetTypeInfoResult(int data_type_filter) {
