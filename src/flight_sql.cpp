5cc80d5897babfb9ab8b94aae479be9ad0e97d4a
Tom Drabas <drabas.t@gmail.com>
2022-04-21 20:10:28 +0000

    Adressing review feedback

--- src/flight_sql.cpp
+++ src/flight_sql.cpp
@@ -9,6 +9,7 @@
 #include <arrow/flight/client.h>
 #include <arrow/flight/sql/client.h>
 #include <arrow/table.h>
+#include <arrow/util/logging.h>
 
 #include "sqlite/sqlite_server.h"
 
@@ -28,30 +29,28 @@ struct createClientParams {
     std::vector<int> skip_queries;
 };
 
-void print_results(
-    const flight::FlightInfo &results, 
-    flightsql::FlightSqlClient client,
+arrow::Status printResults(
+    std::unique_ptr<flight::FlightInfo> &results, 
+    std::unique_ptr<flightsql::FlightSqlClient> &client,
     const flight::FlightCallOptions &call_options) {
         // Fetch each partition sequentially (though this can be done in parallel)
-        for (const flight::FlightEndpoint& endpoint : results.endpoints()) {
+        for (const flight::FlightEndpoint& endpoint : results->endpoints()) {
             // Here we assume each partition is on the same server we originally queried, but this
             // isn't true in general: the server may split the query results between multiple
             // other servers, which we would have to connect to.
 
             // The "ticket" in the endpoint is opaque to the client. The server uses it to
             // identify which part of the query results to return.
-            auto stream_result = client.DoGet(call_options, endpoint.ticket);
-            std::unique_ptr<arrow::flight::FlightStreamReader> stream;
-            if (stream_result.ok()) {
-                stream = std::move(stream_result.ValueOrDie());
-            }
+            ARROW_ASSIGN_OR_RAISE(auto stream, client->DoGet(call_options, endpoint.ticket));
             // Read all results into an Arrow Table, though we can iteratively process record
             // batches as they arrive as well
-            auto table_result = stream->ToTable();
-            auto table = std::move(table_result.ValueOrDie());
+            ARROW_ASSIGN_OR_RAISE(auto table, stream->ToTable());
+            // auto table_result = stream->ToTable();
+            // auto table = std::move(table_result.ValueOrDie());
 
             std::cout << table->ToString() << std::endl;
         }
+    return arrow::Status::OK();
 }
 
 std::string readFileIntoString(const std::string& path) {
@@ -76,11 +75,11 @@ bool checkIfSkip(std::string path, int query_id) {
     return false;
 }
 
-void runQueries(
-        flightsql::FlightSqlClient client, 
+arrow::Status runQueries(
+        std::unique_ptr<flightsql::FlightSqlClient> &client, 
         const std::string &query_path, 
         const std::vector<int> &skip_queries, 
-        flight::FlightCallOptions call_options
+        flight::FlightCallOptions &call_options
     ) {
     int skip_vector_it = 0;
     for (const auto & file : std::filesystem::directory_iterator(query_path)) {
@@ -94,135 +93,74 @@ void runQueries(
         std::string kQuery = readFileIntoString(file.path());
 
         std::cout << "Executing query: '" << kQuery << "'" << std::endl;
-        auto flight_info_result = client.Execute(call_options, kQuery);
-        std::unique_ptr<flight::FlightInfo> flight_info;
+        ARROW_ASSIGN_OR_RAISE(auto flight_info, client->Execute(call_options, kQuery));
 
-        if (flight_info_result.ok()) {
-            flight_info = std::move(flight_info_result.ValueOrDie());
-            print_results(*flight_info, client, call_options);
-        } else {
-            std::cout << "There was a problem executing this query..." << std::endl;
+        if (flight_info != nullptr) {
+            printResults(flight_info, client, call_options);
         }
     }
+
+    return arrow::Status::OK();
 }
 
-void* CreateServer(void *params) {
-    // unpack the args
-    struct createServerParams *csp = (struct createServerParams*) params;
-    long id = csp->id;
-    std::string db_path = csp->db_path;
-
-    std::cout << "Creating server in thread: " << (long) id << std::endl;
-
-    auto location_result = flight::Location::ForGrpcTcp("localhost", port);
-    arrow::flight::Location location;
-    if (location_result.ok()) {
-        location = std::move(location_result).ValueOrDie();
-    } else {
-        std::cout << "Error occured when assigning location. Stopping." << std::endl;
-        return (void*) 1;
-    }
-
+arrow::Result<std::shared_ptr<arrow::flight::FlightServerBase>> CreateServer(std::string db_path) {
+    ARROW_ASSIGN_OR_RAISE(auto location,
+                        arrow::flight::Location::ForGrpcTcp("0.0.0.0", port));
     arrow::flight::FlightServerOptions options(location);
-    auto server_result = arrow::flight::sql::sqlite::SQLiteFlightSqlServer::Create(db_path);
+
     std::shared_ptr<arrow::flight::sql::sqlite::SQLiteFlightSqlServer> server;
+    ARROW_ASSIGN_OR_RAISE(server,
+                            arrow::flight::sql::sqlite::SQLiteFlightSqlServer::Create(db_path));
 
-    if (server_result.ok()) {
-        server = std::move(server_result).ValueOrDie();
-    } else {
-        std::cout << "Error creating server. Stopping." << std::endl;
-        return (void*) 1;
-    }
-
-    if (!server->Init(options).ok()) {
-        std::cout << "Error initializing database. Stopping." << std::endl;
-        return (void*) 1;
-    }
+    ARROW_CHECK_OK(server->Init(options));
     // Exit with a clean error code (0) on SIGTERM
-    if (!server->SetShutdownOnSignals({SIGTERM}).ok()) {
-        std::cout << "Error assigning shutdown signal. Stopping." << std::endl;
-        return (void*) 1;
-    }
+    ARROW_CHECK_OK(server->SetShutdownOnSignals({SIGTERM}));
 
     std::cout << "Server listening on localhost:" << server->port() << std::endl;
-    if (!server->Serve().ok()) {
-        std::cout << "Error serving the data. Stopping." << std::endl;
-        return (void*) 1;
-    }
-    return (void*) 0;
+    return server;
 }
 
-void* CreateClient(void *params) {
-    // unpack the args
-    struct createClientParams *ccp = (struct createClientParams*) params;
-    long id = ccp->id;
-    std::string query_path = ccp->query_path;
-    std::vector<int> skip_queries = ccp->skip_queries;
-    std::cout << "Creating client in thread: " << (long) id << std::endl;
+arrow::Result<std::unique_ptr<flightsql::FlightSqlClient>> CreateClient(std::string query_path, std::vector<int> skip_queries) {
+    ARROW_ASSIGN_OR_RAISE(auto location,
+                        arrow::flight::Location::ForGrpcTcp("localhost", port));
+    arrow::flight::FlightServerOptions options(location);
 
-    auto location_result = flight::Location::ForGrpcTcp("localhost", port);
-    arrow::flight::Location location;
-    if (location_result.ok()) {
-        location = std::move(location_result).ValueOrDie();
-    } else {
-        std::cout << "Error occured when assigning location. Stopping.";
-        return (void*) 1;
-    }
-    std::cout << "Connecting to " << location.ToString() << std::endl;
+    ARROW_ASSIGN_OR_RAISE(auto flight_client, flight::FlightClient::Connect(location));
+    std::cout << "Connected to server: localhost:" << port << std::endl; 
 
-    // Set up the Flight SQL client
-    auto flight_client_result = flight::FlightClient::Connect(location);
-    std::unique_ptr<flight::FlightClient> flight_client;
-    
-    if (flight_client_result.ok()) {
-        flight_client = std::move(flight_client_result.ValueOrDie());
-    }
     std::unique_ptr<flightsql::FlightSqlClient> client(
         new flightsql::FlightSqlClient(std::move(flight_client)));
+    std::cout << "Client created." << std::endl;
+
+    return client;
+}
+
+arrow::Status Main() {
+    std::string db_path = "../data/TPC-H-small.db";
+    ARROW_ASSIGN_OR_RAISE(auto server, CreateServer(db_path));
+
+    std::string query_path = "../queries/sqlite";
+    std::vector<int> skip_queries = {17}; // the rest of the code assumes this is ORDERED vector!
+    ARROW_ASSIGN_OR_RAISE(auto client, CreateClient(query_path, skip_queries));
 
     flight::FlightCallOptions call_options;
-    auto tables_result = client->GetTables(call_options, NULL, NULL, NULL, NULL, NULL);
-    std::unique_ptr<flight::FlightInfo> tables;
+    ARROW_ASSIGN_OR_RAISE(std::unique_ptr<flight::FlightInfo> tables, client->GetTables(call_options, NULL, NULL, NULL, NULL, NULL));
 
-    if (tables_result.ok()) {
-        tables = std::move(tables_result.ValueOrDie());
-        print_results(*tables, *client, call_options);
-    } else {
-        std::cout << "We got a problem" << std::endl;
-        return (void*) 1;
+    if (tables != nullptr) {
+        printResults(tables, client, call_options);
     }
 
-    runQueries(*client, query_path, skip_queries, call_options);
+    runQueries(client, query_path, skip_queries, call_options);
 
-    pthread_exit(NULL);
+    return arrow::Status::OK();
 }
 
 int main(int argc, char** argv) {
-    pthread_t threads[2]; // two threads: server and client
-    void* result_server; 
-    void* result_client;
-
-    createServerParams csp;
-    csp.id = 0;
-    csp.db_path = "../data/TPC-H-small.db";
-    
-    createClientParams ccp;
-    ccp.id = 1;
-    ccp.query_path = "../queries/sqlite";
-    ccp.skip_queries = {17}; // the rest of the code assumes this is ORDERED vector!
-
-    int server_t = pthread_create(&threads[0], NULL, CreateServer, &csp);
-    usleep(2000); // let the server start up
-
-    int client_t = pthread_create(&threads[1], NULL, CreateClient, &ccp);
-
-    if (client_t != 0) {
-        return EXIT_FAILURE;
+    auto status = Main();
+    if (!status.ok()) {
+        std::cerr << status << std::endl;
+        return 1;
     }
 
-    pthread_join(threads[1], &result_client);
-    std::raise(SIGTERM); // shutdown the server
-
-    pthread_join(threads[0], &result_server);
     return EXIT_SUCCESS;
 }
