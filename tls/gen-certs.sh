7bbaf289252dc6b99b361d7ca0d10a68bc5da667
prmoore77 <prmoore77@hotmail.com>
2023-08-02 16:15:21 -0400

    Now generating version 3 TLS certificates

--- tls/gen-certs.sh
+++ tls/gen-certs.sh
@@ -5,21 +5,23 @@ set -eux
 openssl genrsa -out root-ca.key 4096
 openssl req -x509 -new -nodes \
         -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=test" \
-        -key root-ca.key -sha256 -days 10000 -out root-ca.pem
+        -key root-ca.key -sha256 -days 10000 -out root-ca.pem -extensions v3_ca
 
 openssl genrsa -out cert0.key 4096
 openssl req -new -sha256 -key cert0.key \
         -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=localhost" \
         -out cert0.csr
-# Convert to PKCS#1 for Java
-openssl pkcs8 -in cert0.key -topk8 -nocrypt > cert0.pkcs1
+
 openssl x509 -req -in cert0.csr -CA root-ca.pem -CAkey root-ca.key -CAcreateserial \
-        -out cert0.pem -days 10000 -sha256
+        -out cert0.pem -days 10000 -sha256 -extfile v3_usr.cnf -extensions v3_usr_extensions
 
 openssl genrsa -out cert1.key 4096
 openssl req -new -sha256 -key cert1.key \
         -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=localhost" \
         -out cert1.csr
-openssl pkcs8 -in cert1.key -topk8 -nocrypt > cert1.pkcs1
 openssl x509 -req -in cert1.csr -CA root-ca.pem -CAkey root-ca.key -CAcreateserial \
-        -out cert1.pem -days 10000 -sha256
+        -out cert1.pem -days 10000 -sha256 -extfile v3_usr.cnf -extensions v3_usr_extensions
+
+# Convert to PKCS#1 for Java
+openssl pkcs8 -in cert0.key -topk8 -nocrypt > cert0.pkcs1
+openssl pkcs8 -in cert1.key -topk8 -nocrypt > cert1.pkcs1
