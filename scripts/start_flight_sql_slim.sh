a5374b59ff63dfaa5fa61b66d8126627ab323af4
prmoore77 <prmoore77@hotmail.com>
2024-04-26 09:59:37 -0400

    Bugfix for slim start script

--- scripts/start_flight_sql_slim.sh
+++ scripts/start_flight_sql_slim.sh
@@ -7,7 +7,7 @@ L_DATABASE_BACKEND=${2:-${DATABASE_BACKEND:-"duckdb"}}
 L_PRINT_QUERIES=${3:-${PRINT_QUERIES:-"1"}}
 L_TLS_ENABLED=${4:-${TLS_ENABLED:-"0"}}
 L_TLS_CERT=${5:-${TLS_CERT:-"tls/cert0.pem"}}
-L_TLS_KEY=${6:-${TLS_CERT:-"tls/cert0.key"}}
+L_TLS_KEY=${6:-${TLS_KEY:-"tls/cert0.key"}}
 
 TLS_ARG=""
 if [ "${L_TLS_ENABLED}" == "1" ]
