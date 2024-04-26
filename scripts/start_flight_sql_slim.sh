5dc60dcf5471ff043de32250d0e5e5dd40de76c0
prmoore77 <prmoore77@hotmail.com>
2024-04-26 09:44:15 -0400

    Adding slim docker image.  Closes #36

---
+++ scripts/start_flight_sql_slim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+L_DATABASE_FILENAME=${1:-${DATABASE_FILENAME?"You must specify a database filename."}}
+L_DATABASE_BACKEND=${2:-${DATABASE_BACKEND:-"duckdb"}}
+L_PRINT_QUERIES=${3:-${PRINT_QUERIES:-"1"}}
+L_TLS_ENABLED=${4:-${TLS_ENABLED:-"0"}}
+L_TLS_CERT=${5:-${TLS_CERT:-"tls/cert0.pem"}}
+L_TLS_KEY=${6:-${TLS_CERT:-"tls/cert0.key"}}
+
+TLS_ARG=""
+if [ "${L_TLS_ENABLED}" == "1" ]
+then
+  TLS_ARG="--tls ${L_TLS_CERT} ${L_TLS_KEY}"
+  popd
+fi
+
+# Setup the print_queries option
+PRINT_QUERIES_FLAG=""
+if [ "${L_PRINT_QUERIES}" == "1" ]
+then
+  PRINT_QUERIES_FLAG="--print-queries"
+fi
+
+flight_sql_server --backend="${L_DATABASE_BACKEND}" --database-filename="${L_DATABASE_FILENAME}" ${TLS_ARG} ${PRINT_QUERIES_FLAG}
